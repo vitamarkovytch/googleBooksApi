a76a9ad95f3e89f02e9227fc8178ee0900dd05d6
Vita <vita.2603.d@gmail.com>
2018-07-22 16:44:41 +0300

    Edit Book Dialog works, Validators work, Pipe and Directive work, the book in Books list update after Edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
 import { BooksListComponent } from './books-list/books-list.component';
 import {MaterialModule} from './shared/modules/material.module';
+import { EnglishLettersPipe } from './shared/pipes/english-letters.pipe';
+import { EditDialogComponent } from './edit-dialog/edit-dialog.component';
+import { DateValidatorDirective } from './shared/directives/date-validator.directive';
+import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksListComponent,
+    EnglishLettersPipe,
+    EditDialogComponent,
+    DateValidatorDirective,
+    DeleteDialogComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
+  entryComponents: [
+    EditDialogComponent,
+    DeleteDialogComponent
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material';
+
 import {ServerService} from '../../services/server.service';
 import {BooksListModel} from '../shared/model/books-list.model';
+import {EditDialogComponent} from '../edit-dialog/edit-dialog.component';
+import {DeleteDialogComponent} from '../delete-dialog/delete-dialog.component';
 
 @Component({
   selector: 'app-books-list',
@@ -10,7 +14,8 @@ import {BooksListModel} from '../shared/model/books-list.model';
 export class BooksListComponent implements OnInit {
   booksList: BooksListModel [] = [];
 
-  constructor(private server: ServerService) {
+  constructor(private server: ServerService,
+              public dialog: MatDialog) {
   }
 
   ngOnInit() {
@@ -20,21 +25,41 @@ export class BooksListComponent implements OnInit {
   getBooks() {
     this.server.getBooksFromGoogle().subscribe(
       data => {
-        // console.log(data);
-
-        this.booksList = data['items'].map((book, id) => {
+        console.log(data);
+        this.booksList = data['items'].map((book) => {
           return new BooksListModel(
-            id,
+            book['id'],
             book['volumeInfo']['authors'],
             book['volumeInfo']['publishedDate'],
             book['volumeInfo']['title'],
           );
         });
-        console.log(this.booksList);
       },
       error => {
         console.log(error);
       }
     );
+  }
+
+  openEditDialog(book: BooksListModel, i: number): void {
+    const dialogRef = this.dialog.open(EditDialogComponent, {
+      width: '350px',
+      data: {book}
+    });
+
+    dialogRef.afterClosed().subscribe((result: BooksListModel) => {
+      if (typeof result === 'undefined') {
+        console.log('failed ' + result);
+      } else {
+        this.booksList[i] = result;
+      }
+    });
+  }
+
+  openDeleteDialog(book, i) {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      width: '350px',
+      data: {book}
+    });
   }
 }
