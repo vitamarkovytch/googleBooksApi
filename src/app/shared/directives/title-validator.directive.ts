a96363b7202d5288b25c1690cadb5916d5eddca5
Vita <vita.2603.d@gmail.com>
2018-07-25 18:41:28 +0300

    Fixed bug when Edit Book Title with entering only not English letters(Russian, Hebrew)

--- src/app/shared/directives/title-validator.directive.ts
+++ src/app/shared/directives/title-validator.directive.ts
@@ -17,16 +17,29 @@ export class TitleValidatorDirective {
       /*MAKING toLowerCase FOR THE RIGHT COMPARISON,
       * .replace(/[^a-zA-Z0-9 ]/g - REPLACE ALL NON ENGLISH SYMBOLS*/
       let result: boolean;
+
+      /*ADD BOOK*/
+      /*FIRST CONDITION UNTIL || CHECKS IF TITLE THAT WE ADD EQUALS TO ANY TITLE IN BOOKS LIST*/
+      /*SECOND CONDITION AFTER || CHECKS IF TITLE THAT WE ADD EQUAL TO EMPTY STRING,
+         BECAUSE IF USER TYPE ONLY NON ENGLISH LETTERS THE STRING BECOMES TO ''*/
       if (book === null) {
          result = booksList.some(function (element) {
           return (element.bookTitle.toLowerCase().replace(/[^a-zA-Z0-9]/g, '') ===
-            control.value.toLowerCase().replace(/[^a-zA-Z0-9]/g, ''));
+            control.value.toLowerCase().replace(/[^a-zA-Z0-9]/g, '') ||
+            control.value.toLowerCase().replace(/[^a-zA-Z0-9]/g, '') === '');
         });
+
+        /*EDIT BOOK*/
+        /*FIRST CONDITION CHECKS IF TITLE THAT WE EDIT EQUALS TO ANY TITLE IN BOOKS LIST AND AT THE
+         SAME TIME THIS TITLE DOESN'T HAVE TO BE THE TITLE OF BOOK THAT WE EDIT*/
+        /*SECOND CONDITION AFTER || CHECKS IF TITLE THAT WE EDIT DOESN'T EQUAL TO EMPTY STRING
+           BECAUSE IF USER TYPE ONLY NON ENGLISH LETTERS THE STRING BECOMES TO ''*/
       } else {
         result = booksList.some(function (element) {
           return (element.bookTitle.toLowerCase().replace(/[^a-zA-Z0-9]/g, '') ===
             control.value.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
-            && element.id !== book.id);
+            && element.id !== book.id
+          || control.value.toLowerCase().replace(/[^a-zA-Z0-9]/g, '') === '');
         });
       }
 
